fdc6b75c5780869b588b8cf75f0893118b8b6c8f
Ninh Huynh <kimninh1610@gmail.com>
2025-04-12 16:52:37 +0700

    add cts test ExtractDecodeEditEncodeMuxTest

--- app/src/androidTest/java/com/android/media/cts/InputSurfaceInterface.java
+++ app/src/androidTest/java/com/android/media/cts/InputSurfaceInterface.java
@@ -1,4 +1,27 @@
 package com.android.media.cts;
 
+import android.media.MediaCodec;
+
+import com.android.grafika.transcode.InputSurface;
+import com.android.grafika.transcode.NdkMediaCodec;
+
+/**
+ * This interface exposes the minimum set of {@link InputSurface} APIs used in {@link EncodeDecodeTest}.
+ */
 public interface InputSurfaceInterface {
+
+    void makeCurrent();
+
+    boolean swapBuffers();
+
+    void setPresentationTime(long nsecs);
+
+    void configure(MediaCodec codec);
+
+    void configure(NdkMediaCodec codec);
+
+    void updateSize(int width, int height);
+
+    void release();
+
 }
